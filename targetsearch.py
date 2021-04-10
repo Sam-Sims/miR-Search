5f1a0d072c38a2aaef5322cdc44d5895d13cd0f8
sammy <samsimss98@gmail.com>
2021-04-10 12:37:06 +0100

    Refactored searching to avoid code reuse / added functions for other targets

--- targetsearch.py
+++ targetsearch.py
@@ -1,29 +1,48 @@
 import re
 
+
 class TargetSearch:
-    def __init__(self, utr_input, mir_input):
+    def __init__(self, utr_input):
         self.utr = utr_input # list of biopython seq objects
-        self.mir = mir_input # search string
 
+    def search_6mer(self, mir):
+        print("Searching for 6mer:", mir)
+        result = self._search(mir)
+        print(result)
+        print(len(result))
 
-    def search_6mer(self):
-        print("Searching for 6mer:", self.mir)
+    def search_7mera1(self, mir):
+        print("Searching for 7mer-a1:", mir)
+        result = self._search(mir)
+        print(result)
+        print(len(result))
+
+    def search_7merm8(self, mir):
+        print("Searching for 7mer-m8: ", mir)
+        result = self._search(mir)
+        print(result)
+        print(len(result))
+
+    def search_8mer(self, mir):
+        print("Searching for 8mer: ", mir)
+        result = self._search(mir)
+        print(result)
+        print(len(result))
+
+    def _search(self, mir):
         master_list = []
         for seq in self.utr:
-            print("Searching ", seq.id, "...")
+            #print("Searching ", seq.id, "...")
             seed_locations = []
-            seq_string = str(seq.seq) # convert to string for regex
-            for i in re.finditer(self.mir, seq_string):
+            seq_string = str(seq.seq)  # convert to string for regex
+            for i in re.finditer(mir, seq_string):
                 _start = i.start()
                 _end = i.end()
                 _seed_locations = [_start, _end]
                 seed_locations.append(seq.id)
                 seed_locations.append(_seed_locations)
             if not seed_locations:
-                print("False")
+                ans = 1+1
             else:
-                print("True")
                 master_list.append(seed_locations)
-        print(master_list)
-        print(len(master_list))
-        print(self.mir)
+        return master_list
