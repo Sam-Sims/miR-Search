01a05b96080010ed90e447a0fcc37a3ad346f3f2
sammy <samsimss98@gmail.com>
2021-04-11 23:00:49 +0100

    removed MicroRNASearch class - moved to targetsearch.py

--- sequence_handler.py
+++ sequence_handler.py
@@ -1,5 +1,6 @@
-from Bio import SeqIO
 import re
+
+from Bio import SeqIO
 
 
 class MicroRNA:
@@ -47,78 +48,11 @@ class MicroRNA:
         return str(self.mir_seq[:7] + "A")
 
 
-class MicroRNASearch: # redundant
-    def __init__(self, input_utr):
-        self.utr = input_utr
-
-    def search_6mer(self, mir):
-        print("Searching for 6mer: ", mir)
-        seed_locations = []
-        for i in re.finditer(mir, self.utr):
-            _start = i.start()
-            _end = i.end()
-            _seed_locations = [_start, _end]
-            seed_locations.append(_seed_locations)
-        if not seed_locations:
-            return False, seed_locations  # still need to return list although empty
-        else:
-            return True, seed_locations  # return tuple - first always bool indicating if seed site exsists, second a list of locations within the utr - if no locations list will be empty
-
-    def search_7merm8(self, mir):  # 7mer-m8 = posistions 2-7 + match at posistion 8
-        print("Searching for 7mer-m8: ", mir)
-        seed_locations = []
-        is_7mera1 = False
-        for i in re.finditer(mir, self.utr):
-            _start = i.start()
-            _end = i.end()
-            _seed_locations = [_start, _end]
-            seed_locations.append(_seed_locations)
-            _end_check_a = _end + 1
-            if self.utr[_start:_end_check_a].endswith('A'):
-                is_7mera1 = True
-            else:
-                is_7mera1 = False
-        if not seed_locations:  # if list empty - no 7mers return false
-            return False, seed_locations  # still need to return list although empty
-        else:  # if list has items - i.e 7mers found
-            if is_7mera1:  # check if A at pos1 in miRNA - if false return false as this would be 7mer-a1
-                return False, seed_locations  # still need to return list although empty
-            else:  # Else must be a 7mer-m8
-                return True, seed_locations
-
-    def search_7mera1(self, mir):  # probably ineffecient searching again when already searched for 7mers - LOOK IN TO
-        print("Searching for 7mer-a1: ", mir)
-        seed_locations = []
-        is_7mera1 = False
-        for i in re.finditer(mir, self.utr):
-            _start = i.start()
-            _end = i.end()
-            _seed_locations = [_start, _end]
-            seed_locations.append(_seed_locations)
-        if not seed_locations:
-            return False, seed_locations
-        else:
-            return True, seed_locations
-
-    def search_8mers(self, mir):
-        print("Searching for 8mer: ", mir)
-        seed_locations = []
-        for i in re.finditer(mir, self.utr):
-            _start = i.start()
-            _end = i.end()
-            _seed_locations = [_start, _end]
-            seed_locations.append(_seed_locations)
-        if not seed_locations:
-            return False, seed_locations  # still need to return list although empty
-        else:
-            return True, seed_locations
-
-
 class FASTAParse:
     def read_mir(self, path):
         for fasta_sequence in SeqIO.parse(open(path), 'fasta'):
             print("Reading miRNA at " + path + "...")
-            print("miRNA: ",  fasta_sequence.seq)
+            print("miRNA: ", fasta_sequence.seq)
             return fasta_sequence.seq
 
     def read_3utr(self, path):
@@ -134,5 +68,3 @@ class FASTAParse:
             print(fasta_sequence.seq)
             utr_list.append(fasta_sequence)
         return utr_list
-
-
