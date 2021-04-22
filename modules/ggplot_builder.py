e1deff816e1157a0a868989ada696168ca81af6d
sammy <samsimss98@gmail.com>
2021-04-22 13:31:49 +0100

    Created ggplot_builder.py
    - Takes miR-Search output and sleuth output (containg log2fc)
    - Combines them into a single csv file for easy plotting in R using ggplot2

---
+++ modules/ggplot_builder.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+
+def prepare_sleuth_results(sleuth_results):
+    df = pd.read_csv(sleuth_results)
+    df = df.drop(df.columns[[0,2, 3, 4,7,8,9,10,11,12]], axis=1)
+    return df
+
+
+def prepare_targets(target_file):
+    df = pd.read_csv(target_file)
+    df = df.drop(df.columns[[1,3,5,7]], axis=1)
+    df['6mer'] = df['6mer'].str[:15]
+    df['7mera1'] = df['7mera1'].str[:15]
+    df['7merm8'] = df['7merm8'].str[:15]
+    df['8mer'] = df['8mer'].str[:15]
+    return df
+
+
+def merge(sleuth, targets):
+    df_temp = targets.drop(targets.columns[[1,2,3]] , axis=1)
+    targets_6mer= pd.merge(sleuth, df_temp, left_on = 'target_id', right_on = '6mer', how = 'inner')
+
+    df_temp = targets.drop(targets.columns[[0, 2, 3]], axis=1)
+    targets_7mera1= pd.merge(sleuth, df_temp, left_on='target_id', right_on='7mera1', how='inner')
+
+    df_temp = targets.drop(targets.columns[[0, 1, 3]], axis=1)
+    targets_7merm8= pd.merge(sleuth, df_temp, left_on='target_id', right_on='7merm8', how='inner')
+
+    df_temp = targets.drop(targets.columns[[0, 1, 2]], axis=1)
+    targets_8mer = pd.merge(sleuth, df_temp, left_on='target_id', right_on='8mer', how='inner')
+
+    targets_6mer['6mer'] = '6mer'
+    targets_6mer.rename(columns={"6mer": "type"}, inplace=True)
+    targets_7mera1['7mera1'] = '7mera1'
+    targets_7mera1.rename(columns={"7mera1": "type"}, inplace=True)
+    targets_7merm8['7merm8'] = '7merm8'
+    targets_7merm8.rename(columns={"7merm8": "type"}, inplace=True)
+    targets_8mer['8mer'] = '8mer'
+    targets_8mer.rename(columns={"8mer": "type"}, inplace=True)
+
+    targets_6mer = targets_6mer[targets_6mer['b'].notna()]
+    targets_7mera1 = targets_7mera1[targets_7mera1['b'].notna()]
+    targets_7merm8 = targets_7merm8[targets_7merm8['b'].notna()]
+    targets_8mer = targets_8mer[targets_8mer['b'].notna()]
+
+    sleuth['se_b'] = 'control'
+    sleuth.rename(columns={"se_b": "type"}, inplace=True)
+    sleuth= sleuth[sleuth['b'].notna()]
+    sleuth = sleuth.drop(sleuth.columns[[0]], axis=1)
+    print(sleuth)
+
+    final_df = pd.concat([targets_6mer, targets_7mera1, targets_7merm8, targets_8mer, sleuth])
+
+    final_df = final_df.drop(final_df.columns[[0,2]], axis=1)
+
+
+    print(final_df)
+    final_df.to_csv("out.csv", index=False)
