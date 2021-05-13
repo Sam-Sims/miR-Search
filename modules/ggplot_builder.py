496cf97f4403704327bc1b6acadb6826a44601de
sammy <samsimss98@gmail.com>
2021-05-13 19:57:26 +0100

    plot builder for icshape data

--- modules/ggplot_builder.py
+++ modules/ggplot_builder.py
@@ -64,3 +64,42 @@ def merge(sleuth, targets, output):
         os.makedirs('ggplot_format_output')
     filename = str("ggplot_format_output/" + output)
     final_df.to_csv(filename, index=False)
+
+def prepare_sleuth_results_icshape(sleuth_results):
+    df = pd.read_csv(sleuth_results)
+    df = df.drop(df.columns[[0,4,5,7,8,9,10,11,12,13]], axis=1)
+    return df
+
+def prepare_shape_data(shape_dict):
+    df_top = pd.DataFrame(shape_dict[0].items(), columns=['transcript', 'shape_score'])
+    df_bot = pd.DataFrame(shape_dict[1].items(), columns=['transcript', 'shape_score'])
+    return df_top, df_bot
+
+def merge_shape(sleuth, shape, output):
+    merge_top = pd.merge(sleuth, shape[0], left_on='target_id', right_on='transcript', how='inner')
+    merge_top = merge_top[merge_top['b'].notna()]
+
+    merge_bot = pd.merge(sleuth, shape[1], left_on='target_id', right_on='transcript', how='inner')
+    merge_bot = merge_bot[merge_bot['b'].notna()]
+    #merge_top = merge_top.drop(merge_top.columns[[0,1,2,4]], axis=1)
+    merge_top['shape_score'] = "High Structured (Top 20%) N = " + str(len(merge_top)) +")"
+    #merge_bot = merge_bot.drop(merge_bot.columns[[0, 1, 2, 4]], axis=1)
+    merge_bot['shape_score'] = "Low Structured (Bottom 20%) N = " + str(len(merge_bot)) + ")"
+
+    sleuth = sleuth[sleuth['b'].notna()]
+    sleuth = sleuth[~sleuth.target_id.isin(merge_top.target_id)]
+    sleuth = sleuth[~sleuth.target_id.isin(merge_bot.target_id)]
+    sleuth['se_b'] = 'non-targets (N= ' + str(len(sleuth)) + ")"
+    sleuth.rename(columns={"se_b": "shape_score"}, inplace=True)
+    print(sleuth)
+
+    final_df = pd.concat([merge_top, merge_bot, sleuth])
+    print(final_df)
+    final_df = final_df.drop(final_df.columns[[0,1,2,4]], axis=1)
+    final_df.rename(columns={"shape_score": "type"}, inplace=True)
+    print(final_df)
+    if not os.path.exists('ggplot_format_output_shape'):
+        os.makedirs('ggplot_format_output_shape')
+    filename = str("ggplot_format_output_shape/" + output)
+    final_df.to_csv(filename, index=False)
+
